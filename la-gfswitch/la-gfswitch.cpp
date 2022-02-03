1acedd6eff8b6373e431d6c136aed1554dbae3df
Roudy <savokmms1@mail.ru>
2022-02-03 16:00:16 +0300

    'v1.0'

---
+++ la-gfswitch/la-gfswitch.cpp
@@ -0,0 +1,54 @@
+﻿#include <windows.h> 
+#include <string>
+#include <thread>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+
+int main()
+{
+    std::string rule_name = "LagSwitch";
+    std::string executable_path = "";
+    std::cout << "Enter game path folder: ";
+    std::cin >> executable_path;
+    int lag_key = VK_F2;
+    int exit_key = VK_END;
+
+
+    int lag_time = 3500;
+
+
+
+
+
+    std::string cmd = "netsh advfirewall firewall delete rule name=\"" + rule_name + "\"";
+    WinExec(cmd.c_str(), 0);
+
+    while (!(GetAsyncKeyState(exit_key) & 0x8000)) { // loop until exit key is pressed
+        if (GetAsyncKeyState(lag_key) & 0x8000) {
+            Beep(800, 200);
+
+
+            cmd = "netsh advfirewall firewall add rule name =\"" + rule_name + "\" dir=out action=block program=\"" + executable_path + "\"";
+            WinExec(cmd.c_str(), 0);
+
+
+            cmd = "netsh advfirewall firewall add rule name =\"" + rule_name + "\" dir=in action=block program=\"" + executable_path + "\"";
+            WinExec(cmd.c_str(), 0);
+
+
+            Sleep(lag_time);
+
+            cmd = "netsh advfirewall firewall delete rule name=\"" + rule_name + "\"";
+            WinExec(cmd.c_str(), 0);
+
+            Beep(500, 200);
+        }
+        Sleep(5);
+    }
+    Beep(200, 200);
+
+    return 0;
+}
